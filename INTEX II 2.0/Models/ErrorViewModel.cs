b4119a3b99cc94976c4e095e9cffccb09c6e9e82
Ethritch <ethritch@gmail.com>
2022-04-04 09:10:26 -0600

    Add project files.

---
+++ INTEX II 2.0/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace INTEX_II_2._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
